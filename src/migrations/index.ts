e8974d0b10dbc6c0e38325c83a0e79394518c401
Mason Rathunde <mjrathunde3@gmail.com>
2026-05-13 20:21:49 -0500

    fix: migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20260427_003218 from './20260427_003218';
 import * as migration_20260430_210945 from './20260430_210945';
 import * as migration_20260509_192418 from './20260509_192418';
+import * as migration_20260514_012102 from './20260514_012102';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20260509_192418.up,
     down: migration_20260509_192418.down,
-    name: '20260509_192418'
+    name: '20260509_192418',
+  },
+  {
+    up: migration_20260514_012102.up,
+    down: migration_20260514_012102.down,
+    name: '20260514_012102'
   },
 ];
